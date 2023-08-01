3b8c401e53c5d791be03eb69490f1938e0a938d1
kkp7700@gmail.com <https://github.com/statpng>
2023-08-01 15:18:28 +0900

    20230801_1518

--- R/operators.R
+++ R/operators.R
@@ -13,3 +13,19 @@
   paste0(x, "/", y)
 }
 
+#' @export png.mat.pairs
+png.mat.pairs <- function(LIST, f=function(x,y){ norm( x-y, "F" ) }){
+  N=length(LIST)
+  
+  # f <- function(x,y){ norm( x-y, "F" ) }
+  Combinations <- combn(1:N, 2)
+  
+  out <- matrix(NA,N,N)
+  for( j in 1:ncol(Combinations) ){
+    row <- Combinations[1,j]
+    col <- Combinations[2,j]
+    out[row,col] <- Reduce(f, LIST[Combinations[,j]])
+    out[col,row] <- out[row,col]
+  }
+  out
+}
