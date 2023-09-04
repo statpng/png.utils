1720f521b5bce0c43241f79b9ec12180ba912e4a
kkp7700@gmail.com <https://github.com/statpng>
2023-09-04 12:43:50 +0900

    20230904_1243

--- R/tba_triangular_plot.R
+++ R/tba_triangular_plot.R
@@ -1,4 +1,4 @@
-data(iris)
-library(ade4)
-tp<-triangle.plot(iris[,c(1,2,4)],cpoint=0,show.position=F)
-points(tp, pch=c(1,2,8)[iris[,5]])
+# data(iris)
+# library(ade4)
+# tp<-triangle.plot(iris[,c(1,2,4)],cpoint=0,show.position=F)
+# points(tp, pch=c(1,2,8)[iris[,5]])
